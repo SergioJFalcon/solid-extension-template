0a80121e60026a046143faaa8286b1da61ffe4f9
Sergio Falcon <SergioJFalcon@gmail.com>
2024-02-05 22:21:37 -0600

    chrome extension components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,7 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx,css,md,mdx,html,json,scss}',
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: 'class',
   theme: {
     extend: {},
