b9e2c7c56dfc770cfd3113efb0744a95f6497413
Sergio Falcon <SergioJFalcon@gmail.com>
2024-02-05 23:09:44 -0600

    fixed dev tools to point to popup's index.html

--- src/pages/devtools/index.ts
+++ src/pages/devtools/index.ts
@@ -1,9 +1,12 @@
-try {
-  chrome.devtools.panels.create(
-    "Dev Tools",
-    "icon-34.png",
-    "src/pages/panel/index.html"
-  );
-} catch (e) {
-  console.error(e);
-}
+// try {
+//   chrome.devtools.panels.create(
+//     "Dev Tools",
+//     "34x34.png",
+//     "src/pages/devtools/panel.html"
+//   );
+// } catch (e) {
+//   console.error("falcon - error: ",e);
+// }
+chrome.devtools.panels.create('Dev tool', '34x34.png', 'src/pages/popup/index.html', () => {
+  console.log('user switched to this panel');
+});
